a9a89118ed8641505b387c794ba6929b3a81e3b3
votomaixe <votomaixe@users.noreply.github.com>
2020-02-03 21:52:31 +0900

    Update VM.rb
    月曜１７時時点の提出内容を反映した

--- VM.rb
+++ VM.rb
@@ -1,88 +1,106 @@
-# 実行例
-# irb
-#require '/Users/workspace/pair.rb'
-  # （↑のパスは、自動販売機ファイルが入っているパスを指定する）
-  # 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
-
-
-
+#require '/Users/workspace/pair
+# 継承やファイル分割の構想は断念したのでrequireは省略
 class VendingMachine
-  # ステップ０　お金の投入と払い戻しの例コード
-  # ステップ１　扱えないお金の例コード
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
+  attr_accessor :slot_money
   MONEY = [10, 50, 100, 500, 1000].freeze
-  # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
-
-
-  def opening
-  puts "お金を入れてください。", "0: グー", "1: チョキ", "2: パー"
-  inputhand = gets.chomp
-    if inputhand == "0" || inputhand == "1" || inputhand == "2"
-      playerway = inputhand.to_i
-      return playerway
+  def initialize
+    @slot_money = 0
+    @inputmoney = 0
+  end
+  def opening1
+    puts "お金を入れてください。#{MONEY}円が使えます。"
+    @inputmoney = gets.chomp
+      if @inputmoney == "10" || @inputmoney == "50" || @inputmoney == "100" ||@inputmoney =="500" || @inputmoney == "1000"
+        @slot_money +=@inputmoney.to_i
+        self.opening2
+      else
+        puts "そのお金は扱えません。"
+        puts "------------------"
+        return self.return_money
+      end
+  end
+  def opening2
+    puts "0: 続けてお金を入れる", "1: 売り物を見る", "2: ジュースを買う", "3: 投入金額の合計を見る", "4: お金を払い戻す"
+    select = gets.chomp
+      if select == "0"
+        self.opening1
+      elsif select == "1"
+        puts "投入金額:#{@slot_money}円, 商品：#{stock.nedan}円、在庫#{stock.zaiko}本"
+        puts "------------------"
+        return self.opening2
+      elsif select == "2"
+        self.purchase
+      elsif select == "3"
+        self.current_slot_money
+        puts "------------------"
+        return self.opening2
+      elsif select == "4"
+        self.return_money
+      else "0から4の数字を入力して下さい。"
+        return self.opening2
+      end
+  end
+# 以下、計算関係のメソッド
+  def current_slot_money
+    puts "現在の投入金額: #{@slot_money}円"
+    puts "------------------"
+    return self.opening2
+  end
+  def purchase
+    if @slot_money> stock.nedan
+      @slot_money -=stock.nedan
+      stock.shukko
+      puts "#{drinks[0]{"コーラ"}}を1本買いました。投入金額: #{@slot_money}円"
+      puts "------------------"
+      return self.opening2
     else
-      puts "0~2の数字を入力してください。"
-      return hand
+      puts "お金が足りません。投入金額: #{@slot_money}円"
+      puts "------------------"
+      return self.opening2
     end
   end
-
-
-  def initialize
-    # 最初の自動販売機に入っている金額は0円
-    @slot_money = 0
-  end
-  # 投入金額の総計を取得できる。sum
-  def current_slot_money
-    # 自動販売機に入っているお金を表示する
-    @slot_money
-  end
-  # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
-  # 投入は複数回できる。
-  def slot_money(money)
-    # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
-    # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
-    return false unless MONEY.include?(money)
-    # 自動販売機にお金を入れる
-    @slot_money += money
-  end
-  # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
+# 書いているうちに他の機能と組み合わせづらくなったのでボツにした
+  # def slot_money(money)
+  #   return false unless MONEY.include?(money)
+  #   @slot_money += money
+  # end
   def return_money
-    # 返すお金の金額を表示する
-    puts @slot_money
-    # 自動販売機に入っているお金を0円に戻す
-    @slot_money = 0
-  end
-
+    puts "#{@inputmoney}円を返却します。"
+    @inputmoney = 0
+    puts "残額: #{@slot_money}円"
+    puts "------------------"
+    return self.opening1
+    end
 end
-
-class
-
-drinks = [{cola: 120}, {cola: 120}, {cola: 120}, {cola: 120}, {cola: 120}    ]
-# redbull, water はこのハッシュに後ろから追加する予定（暫定）
-
-
-
+# drinksは配列内ハッシュじゃないほうが管理しやすいかった
 class Stock
-
-
   def shukko
-
-
-
+   drinks.delete[0]{:"コーラ"}
+     if false == drinks.delete[0]{"コーラ"} || nil == drinks.delete[0]{:"コーラ"}
+       puts "売り切れです。"
+       return vm.opening2
+     else
+       return vm.opening2
+     end
   end
-
-  def nyuko
-
+  # def nyuko
+  #
+  #   end
+  def nedan
+    kakaku = drinks[0]{:"コーラ"}
+    return kakaku
+  end
+  def zaiko
+    nokori = drinks.count[0]{:"コーラ"}
+    return nokori
+  end
 end
-
-
-end
-
-
+drinks = [ {"コーラ": 120},
+           {"コーラ": 120},
+           {"コーラ": 120},
+           {"コーラ": 120},
+           {"コーラ": 120} ]
+# redbull, water はこのハッシュに後ろから追加する予定（暫定）
 vm = VendingMachine.new
-  # 作成した自動販売機に100円を入れる
-#vm.slot_money (100)
-  # 作成した自動販売機に入れたお金がいくらかを確認する（表示する）
-vm.current_slot_money
-  # 作成した自動販売機に入れたお金を返してもらう
-vm.return_money
+stock = Stock.new
+puts vm.opening1
